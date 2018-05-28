475b6b24ef90d4331f3ca554918a268026942390
Eric Jans <EPMA.Jans@student.han.nl>
2018-05-28 10:08:52 +0200

    Modified code to accept user input

--- RandomForest.py
+++ RandomForest.py
@@ -40,3 +40,42 @@ clf = RandomForestClassifier(n_estimators=25, random_state=0)
 clf.fit(train_features, train_labels)
 pred = clf.predict(test_features)
 print('Accuraatheid is: ' + str(metrics.accuracy_score(test_labels, pred)))
+
+while True:
+    print("\nWat is jouw geslacht? (0 voor VROUW, 1 voor MAN)")
+    geslacht = int(input(""))
+
+    print("\nWat is jou leeftijd?")
+    leeftijd = int(input(""))
+
+    symptoms = None
+    while symptoms is None:
+        print("\nDe beschikbare symptomen zijn:")
+        print(", ".join(available_training_symptoms))
+        print("\nVul je symptomen in, gescheiden door een comma:")
+        symptoms = list(map(lambda v: v.strip().lower(), input("").split(",")))
+
+        existing_symptoms = list(
+            filter(lambda v: v in available_training_symptoms, symptoms))
+
+        if len(existing_symptoms) != len(symptoms):
+            print("\nU mag alleen symptomen opnoemen die bij ons geregistreerd zijn. De symptomen die u invulde maar niet bij ons geregistreerd staan zijn:")
+            not_existing_symptoms = list(
+                set(symptoms) - set(existing_symptoms))
+            print(", ".join(not_existing_symptoms))
+
+            symptoms = None
+    symptoms_array = [geslacht, leeftijd]
+    for available_symptom in available_training_symptoms:
+        symptoms_array.append(1 if available_symptom in symptoms else 0)
+
+    prediction = int(clf.predict([symptoms_array])[0])
+
+    print("\n\n--> De applicatie geeft aan dat u de volgende ziekte heeft:")
+    print(available_diseases[prediction])
+    print("\nUw ziekte is bepaald. Druk op 'j' om de applicatie te herstarten.")
+    again = input("")
+    if again.upper() != "J":
+        break
+
+print("\nU heeft aangegeven dat u geen behoefte meer heeft om de applicatie te herstarten. Fijne dag nog!")
